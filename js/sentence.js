17fcea3317bd4528d9c83358fbfa7a6a8b6c886b
kidevelop <hjh010501@naver.com>
2019-07-15 09:57:36 +0900

    finish

--- js/sentence.js
+++ js/sentence.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
 
-function addSentence() {
+async function addSentence() {
     if(event.keyCode == 13){
         let sentence = document.getElementById('ori-sentence-input').value;
         document.getElementById('ori-sentence-list').innerHTML += '<div class="ori-sentence">' + sentence  + '</div>';
-        let result = sentenceClassification(sentence);
+        let result = await sentenceClassification(sentence);
+        console.log(result)
         if(result['data'] === -1) {
             document.getElementById('result-sentence-list').innerHTML += '<div class="ori-sentence">에러가 발생하였습니다.</div>';
         } else if(result['data'] === 0) {
@@ -12,9 +13,9 @@ function addSentence() {
         } else if(result['data'] === 1 ) {
             document.getElementById('result-sentence-list').innerHTML += '<div class="ori-sentence bad">' + result['text'] + '</div>';
         }
-
+        document.getElementById('ori-sentence-list').scrollTop = document.getElementById('ori-sentence-list').scrollHeight;
+        document.getElementById('result-sentence-list').scrollTop = document.getElementById('result-sentence-list').scrollHeight;
         document.getElementById('ori-sentence-input').value = '';
-        
    }
 }
 
@@ -29,10 +30,11 @@ async function sentenceClassification (data) {
       if (res.status !== 200) {
         return -1
       }
+      console.log(res.data)
       let result = 0
-      if (res.data === 0) {
+      if (res.data == "욕아님") {
         result = 0
-      } else if (res.data === 1) {
+      } else if (res.data == "욕") {
         result = 1
       }
       return { data: result, text: data }
